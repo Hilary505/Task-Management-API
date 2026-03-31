265a8cd4b8e587ca7306b2bb31a9b183af329c1c
Hilary505 <omondihilary19@gmail.com>
2026-03-31 15:33:27 +0300

    modify: database.php file

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 return [
     'host'     => getenv('DB_HOST') ?: '127.0.0.1',
     'port'     => getenv('DB_PORT') ?: '3306',
-    'dbname'   => getenv('DB_NAME') ?: 'task_management',
-    'username' => getenv('DB_USER') ?: 'root',
-    'password' => getenv('DB_PASS') ?: '',
+    'dbname'   => getenv('DB_NAME') ?: 'taskmanager',
+    'username' => getenv('DB_USER') ?: 'taskuser',
+    'password' => getenv('DB_PASS') ?: '1234',
 ];
