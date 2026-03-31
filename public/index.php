bbac6c4bc3fb33548957ccecd8eee60c7ec445cf
Hilary505 <omondihilary19@gmail.com>
2026-03-31 11:35:56 +0300

    feat: add index.php file

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
+require_once __DIR__ . '/../routes/api.php';
