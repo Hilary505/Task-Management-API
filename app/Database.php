c9f9934575d7a69e7e5dddee880576109f9b2d4a
Hilary505 <omondihilary19@gmail.com>
2026-03-31 11:33:33 +0300

    add app/Database.php

---
+++ app/Database.php
@@ -0,0 +1,19 @@
+<?php
+
+class Database
+{
+    private static ?PDO $instance = null;
+
+    public static function connect(): PDO
+    {
+        if (self::$instance === null) {
+            $cfg = require __DIR__ . '/../config/database.php';
+            $dsn = "mysql:host={$cfg['host']};port={$cfg['port']};dbname={$cfg['dbname']};charset=utf8mb4";
+            self::$instance = new PDO($dsn, $cfg['username'], $cfg['password'], [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ]);
+        }
+        return self::$instance;
+    }
+}
